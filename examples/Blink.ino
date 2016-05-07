ee069973bc7e7f58b4685ab2d3de6e3f1532207a
Hans Scharler <hscharler@gmail.com>
2016-05-07 14:41:24 -0400

    Create Blink.ino

---
+++ examples/Blink.ino
@@ -0,0 +1,19 @@
+/*
+  Blink
+  Flashes the Onboard LED of the MKR1000 board connected to pin 6.
+ */
+
+void setup() {
+  // initialize digital pin 6 as an output
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  // turn on LED
+  digitalWrite(6, HIGH);
+  delay(300);
+  
+  // turn off LED
+  digitalWrite(6);
+  delay(300);
+}
